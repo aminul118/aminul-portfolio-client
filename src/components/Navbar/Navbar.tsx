3e865df6a5d68b3b505ad59ea150860efec224c1
MD AMINUL ISLAM <mr.aminul118@gmail.com>
2025-08-01 23:47:54 +0600

    Hamburger icon change

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Menu, X } from 'lucide-react';
+import { Fade as Hamburger } from 'hamburger-react';
 import Link from 'next/link';
 import { useState } from 'react';
 import AminulLogo from '../common/AminulLogo';
@@ -36,7 +36,7 @@ const Navbar = () => {
 
   return (
     <header className="bg-slate-900 fixed top-0 left-0 w-full z-50 shadow-md">
-      <nav className="w-full px-2 md:px-0 py-4 container mx-auto text-white flex items-center justify-between">
+      <nav className="w-full px-2 md:px-0 py-4 container mx-auto flex items-center justify-between">
         {/* Left: Logo */}
         <AminulLogo />
 
@@ -44,23 +44,26 @@ const Navbar = () => {
         <ul className="hidden md:flex space-x-6 text-sm font-medium text-gray-300">
           {navItems.map(({ title, href }) => (
             <li key={title}>
-              <Link href={href} className="hover:text-white transition-colors">
+              <Link href={href} className="hover:primary/80 transition-colors">
                 {title}
               </Link>
             </li>
           ))}
         </ul>
 
-        {/* Mobile Menu Icon */}
+        {/* Mobile Hamburger Icon */}
         <div className="md:hidden">
-          <button onClick={() => setMenuOpen(!menuOpen)}>
-            {menuOpen ? <X size={24} /> : <Menu size={24} />}
-          </button>
+          <Hamburger
+            toggled={menuOpen}
+            toggle={setMenuOpen}
+            size={24}
+            color="#fff"
+          />
         </div>
 
         {/* Mobile Menu */}
         {menuOpen && (
-          <div className="absolute top-16 left-0 w-full bg-[#050B16] text-white flex flex-col items-center space-y-4 py-4 md:hidden z-40 shadow-md">
+          <div className="absolute top-16 left-0 w-full bg-slate-950 text-primary/80 flex flex-col items-center space-y-4 py-6 md:hidden z-40 shadow-md">
             {navItems.map(({ title, href }) => (
               <Link
                 key={title}
